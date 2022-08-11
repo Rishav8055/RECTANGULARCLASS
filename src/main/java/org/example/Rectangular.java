c5358b9849c5824f48c8ececcae798abcc8e9bc8
rishavemail786 <rishavemail786@gmail.com>
2022-08-11 14:39:14 +0530

    SOLUTION DONE

--- src/main/java/org/example/Rectangular.java
+++ src/main/java/org/example/Rectangular.java
@@ -1,2 +1,27 @@
-package org.example;public class Rectangular {
+package org.example;
+
+public class Rectangular {
+    double height;
+    double width;
+
+    Rectangular(){
+        height=1;
+        width=1;
+        System.out.println("Rectangular height = "+this.height+ "width  = "+this.width);
+    }
+
+    public Rectangular(double height, double width) {
+        height = height;
+        width = width;
+        System.out.println("Rectangular height = "+this.height+ "width  = "+this.width);
+    }
+    double getArea(){
+        double rectangularArea=height*width;
+        return rectangularArea;
+
+    }
+    double getPerimeter(){
+        double rectangularPerimeter= 2*(height+width);
+        return rectangularPerimeter;
+    }
 }
--- src/main/java/org/example/RectangularIMPL.java
+++ src/main/java/org/example/RectangularIMPL.java
@@ -1,7 +1,21 @@
 package org.example;
 
-public class Main {
+public class RectangularIMPL {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Rectangular rectangular1 = new Rectangular();
+        System.out.println("Rectangular Area is  =  "+rectangular1.getArea());
+        System.out.println("perimeter        is  =  "+rectangular1.getPerimeter());
+
+
+        Rectangular rectangular2 = new Rectangular(40,4);
+        System.out.println("Rectangular Area is  =  "+rectangular2.getArea());
+        System.out.println("perimeter        is  =  "+rectangular2.getPerimeter());
+
+
+        Rectangular rectangular3 = new Rectangular(35.9,3.5);
+        System.out.println("Rectangular Area is  =  "+rectangular3.getArea());
+        System.out.println("perimeter        is  =  "+rectangular3.getPerimeter());
+
+
     }
 }
